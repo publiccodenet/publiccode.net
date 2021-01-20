afde7d686c8c5a5b564da586c51458274ce78301
Eric Herman <eric@freesa.org>
2021-01-20 12:51:24 +0100

    Adjust htmlproofer syntax
    This uses the PCRE-like slash-bounding for matches, separated by
    commas. The urls now ignore any github edit link and any link with
    twitter.com in it.

--- script/test.sh
+++ script/test.sh
@@ -7,6 +7,5 @@ bundle exec jekyll build
 # Check for broken links and missing alt tags, ignore edit links to GitHub as they might not exist yet
 
 bundle exec htmlproofer \
-    --url-ignore "/github.com/(.*)/edit/" \
-    --url-ignore "/#/" \
+    --url-ignore "/github.com\/(.*)\/edit/,/twitter.com/" \
     ./_site
